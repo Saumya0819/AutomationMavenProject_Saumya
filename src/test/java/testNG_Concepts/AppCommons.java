f7483ec359c76016ea7ef2bb88ee50f1d8ebdb56
Saumya0819 <sranjan.hati@gmail.com>
2025-06-08 19:12:28 +0530

    TestNG pluins added

--- src/test/java/testNG_Concepts/AppCommons.java
+++ src/test/java/testNG_Concepts/AppCommons.java
@@ -1,10 +1,24 @@
 package testNG_Concepts;
 
-public class AppCommons {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class AppCommons extends WebCommons{
+	
+	@Test
+	public void TestCase1() {
+		Assert.fail("Test Case 1 - Home Page Validations Failed..."); // Simulating a failure for demonstration
+		System.out.println("Test Case 1 - Home Page Validations Executed Successfully...");
+	}
+	
+	@Test
+	public void TestCase2() {
+		System.out.println("Test Case 2 - Add Product to Cart Feature Validations Executed Successfully...");
+	}	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@Test(dataProvider = "loginData")
+	public void TestCase3(String username, String password) {
+		System.out.println("Test Case 3 - Login Validations Executed Successfully with..."+username+" ,"+password);
 	}
 
 }
+
